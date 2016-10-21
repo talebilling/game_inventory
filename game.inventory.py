16e38a24ebf30e58283964601bc8f09c96f4d4bf
Emese Billing <talebilling@gmail.com>
2016-10-21 18:08:32 +0200

    add input from file

--- game.inventory.py
+++ game.inventory.py
@@ -4,28 +4,45 @@ Game Inventory
 
 def display_inventory(inv):
     item_numbers = list(inv.values())
-    total_number_of_items = sum(item_numbers)
+    total_number_of_items = sum(item_numbers) 
 
     print("\nInventory: ")
     for k, v in inv.items():
         print("{} {}" .format(v, k))
-    print("Total number of items:", total_number_of_items)
+    print("Total number of items:", total_number_of_items, "\n")
 
-def add_to_inventory(added_items, inv):
+def add_to_inventory(inv, added_items):
     for item in added_items:
         if inv.get(item):
-            inv[item] += 1  #print("Value : {}" .format(inventory.get(item)))
+            inv[item] += 1
         else:
             inv.update({item : 1})
     return inv
 
+def import_inventory(filename, lines): #but by default it's "import_inventory.csv"
+    database = open(filename, "r+")
+    lines = database.readlines()
+    lines = lines[1:]
 
+    #split strings
+    lines = [i.split("\n")[0] for i in lines if "\n" in i]
+    print(lines)
+    database.close()
+    return lines
 
 #main
 #definitions
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} #the player’s inventory, keys = strings, values of items, value = integer (how many of that item)
+inventory = {'rope': 1, 
+            'torch': 6, 
+            'gold coin': 42, 
+            'dagger': 1, 
+            'arrow': 12}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-order = 9
+filename = "input_file.csv"
+lines = []
 
-inventory = add_to_inventory(dragon_loot, inventory)
+inventory = add_to_inventory(inventory, dragon_loot) #add items to inv.
 display_inventory(inventory) #printing out the inventory
+lines = import_inventory(filename, lines)
+inventory = add_to_inventory(inventory, lines)
+display_inventory(inventory)
