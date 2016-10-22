6f4623723f55e4f68162918b4ee7813cd14395a4
Emese Billing <talebilling@gmail.com>
2016-10-22 10:13:33 +0200

    split function, default file, before refactor

--- game.inventory.py
+++ game.inventory.py
@@ -9,24 +9,23 @@ def add_to_inventory(inv, added_items):
             inv[item] += 1
         else:
             inv.update({item : 1})
+    display_inventory(inv)
     return inv
 
-def import_inventory(filename, item_list): #but by default it's "import_inventory.csv"
+def import_inventory(filename="import_inventory.csv"):
     database = open(filename, "r+")
     item_list = database.readlines()
-    item_list = item_list[1:]
-    #split strings
-    item_list = [i.split("\n")[0] for i in item_list if "\n" in i]
     database.close()
-    return item_list
+    split_strings(inventory, item_list)
 
-def add_inventory_from_import(inv, item_list):
-    #convert list to dict
+def split_strings(inv, item_list):
+    item_list = item_list[1:]
+    item_list = [i.split("\n")[0] for i in item_list if "\n" in i]
     item_list = [i.split(",", 1) for i in item_list if "," in i]
+    print(item_list)
     ##should change the values to int
     item_dic = dict(item_list)
-    #add to inventory
-    return inv
+    return inv, item_list
 
 def display_inventory(inv):
     item_numbers = list(inv.values())
@@ -46,9 +45,7 @@ inventory = {'rope': 1,
             'arrow': 12}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 filename = "input_file.csv"
-items_from_file = []
 
-inventory = add_to_inventory(inventory, dragon_loot) #add items to inv.
-items_from_file = import_inventory(filename, items_from_file)
-add_inventory_from_import(inventory, items_from_file)
+#add items to inventory
+inventory = add_to_inventory(inventory, dragon_loot) 
 display_inventory(inventory) #printing out the inventory
