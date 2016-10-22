17ecbf585b6361ae66f211478c06f1fda4ac417e
Emese Billing <talebilling@gmail.com>
2016-10-22 13:44:58 +0200

    merge to inventory

--- game.inventory.py
+++ game.inventory.py
@@ -3,34 +3,42 @@ Game Inventory
 by Emese Billing, Oct 2016
 """
 
-def add_to_inventory(inv, added_items):
+def add_to_inventory(inv, added_items): # todo rename item to item_key
     for item in added_items:
         if inv.get(item):
             inv[item] += 1
         else:
             inv.update({item : 1})
-    display_inventory(inv)
-    return inv
 
-def import_inventory(filename="import_inventory.csv"):
+def merge_to_inventory(inv_to_merge):
+    dictionary_keys_list = inv_to_merge.keys()    
+    for dic_key in dictionary_keys_list:
+        if inventory.get(dic_key):
+            inventory[dic_key] += inv_to_merge[dic_key]
+        else: 
+            inventory.update({dic_key : inv_to_merge[dic_key]})
+
+def import_inventory(filename="import_inventory.csv"): #put file path in code
     database = open(filename, "r+")
     item_list = database.readlines()
     database.close()
-    split_strings(inventory, item_list)
+    inv_to_merge = split_strings(item_list)
+    merge_to_inventory(inv_to_merge)
 
-def split_strings(inv, item_list):
+def split_strings(item_list):
     item_list = item_list[1:]
-    item_list = [i.split("\n")[0] for i in item_list if "\n" in i]
-    item_list = [i.split(",", 1) for i in item_list if "," in i]
-    print(item_list)
-    ##should change the values to int
+    item_list = [i.split(",") for i in item_list]
     item_dic = dict(item_list)
-    return inv, item_list
+    dictionary_keys_list = item_dic.keys()
+    for dic_key in dictionary_keys_list:
+        value = item_dic.get(dic_key)
+        value = int(value.strip())
+        item_dic[dic_key] = value
+    return item_dic
 
 def display_inventory(inv):
     item_numbers = list(inv.values())
     total_number_of_items = sum(item_numbers) 
-    #print Inventory
     print("\nInventory: ")
     for k, v in inv.items():
         print("{} {}" .format(v, k))
@@ -46,6 +54,8 @@ inventory = {'rope': 1,
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 filename = "input_file.csv"
 
-#add items to inventory
-inventory = add_to_inventory(inventory, dragon_loot) 
-display_inventory(inventory) #printing out the inventory
+
+add_to_inventory(inventory, dragon_loot) 
+import_inventory()
+import_inventory(filename)
+display_inventory(inventory)
