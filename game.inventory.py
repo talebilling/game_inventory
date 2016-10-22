11b4e670db2fc5aaf432d9c87012e3eac6bddb05
Emese Billing <talebilling@gmail.com>
2016-10-22 19:36:56 +0200

    export function

--- game.inventory.py
+++ game.inventory.py
@@ -9,14 +9,7 @@ def add_to_inventory(inv, added_items): # todo rename item to item_key
             inv[item] += 1
         else:
             inv.update({item : 1})
-
-def merge_to_inventory(inv_to_merge):
-    dictionary_keys_list = inv_to_merge.keys()    
-    for dic_key in dictionary_keys_list:
-        if inventory.get(dic_key):
-            inventory[dic_key] += inv_to_merge[dic_key]
-        else: 
-            inventory.update({dic_key : inv_to_merge[dic_key]})
+    display_inventory(inventory)
 
 def import_inventory(filename="import_inventory.csv"): #put file path in code
     database = open(filename, "r+")
@@ -36,6 +29,15 @@ def split_strings(item_list):
         item_dic[dic_key] = value
     return item_dic
 
+def merge_to_inventory(inv_to_merge):
+    dictionary_keys_list = inv_to_merge.keys()    
+    for dic_key in dictionary_keys_list:
+        if inventory.get(dic_key):
+            inventory[dic_key] += inv_to_merge[dic_key]
+        else: 
+            inventory.update({dic_key : inv_to_merge[dic_key]})
+    display_inventory(inventory)
+    
 def display_inventory(inv):
     item_numbers = list(inv.values())
     total_number_of_items = sum(item_numbers) 
@@ -43,6 +45,20 @@ def display_inventory(inv):
     for k, v in inv.items():
         print("{} {}" .format(v, k))
     print("Total number of items:", total_number_of_items, "\n")
+
+def export_inventory(filename="export_inventory.csv"): #export all inventory items to a file
+    export_file = open(filename, "w+")
+    export_lines = list_convert_to_string()
+    export_file.writelines(export_lines)
+    export_file.close() 
+
+def list_convert_to_string():
+    first_line_to_export = "item_name,count\n"
+    export_lines = [first_line_to_export]
+    dictionary_keys_list = inventory.keys()   
+    for key in dictionary_keys_list:
+        export_lines.append(key + "," + str(inventory[key]) + "\n")
+    return export_lines
 
 #main
 #definitions
@@ -54,8 +70,8 @@ inventory = {'rope': 1,
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 filename = "input_file.csv"
 
-
-add_to_inventory(inventory, dragon_loot) 
-import_inventory()
-import_inventory(filename)
 display_inventory(inventory)
+add_to_inventory(inventory, dragon_loot) 
+#calling the import function without argument, for reach default file
+import_inventory() 
+export_inventory()
