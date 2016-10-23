946b3142501f5f9c2c46730bde25c0823c3b1995
Emese Billing <talebilling@gmail.com>
2016-10-23 18:58:44 +0200

    small refact

--- game.inventory.py
+++ game.inventory.py
@@ -12,7 +12,7 @@ def add_to_inventory(inv, added_items): # todo rename item to item_key
             inv.update({item : 1})
     display_inventory(inventory)
 
-def import_inventory(dir_path, filename="import_inventory.csv"):
+def import_inventory(filename="import_inventory.csv"):
     database = open(dir_path + "/" + filename, "r+")
     item_list = database.readlines()
     database.close()
@@ -39,7 +39,7 @@ def merge_to_inventory(inv_to_merge):
             inventory.update({dic_key : inv_to_merge[dic_key]})
     display_inventory(inventory)
     
-def export_inventory(dir_path, filename="export_inventory.csv"):
+def export_inventory(filename="export_inventory.csv"):
     export_file = open(dir_path + "/" + filename, "w+")
     inventory_key_list = inventory.keys() 
     export_lines = list_convert_to_string(inventory_key_list)
@@ -91,21 +91,21 @@ def order_inventory_items(order):
         inventory_items = sorted(inventory.items(), key = lambda item: item[0], reverse=True)
     return inventory_items
 
+def demo():
+    dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+    filename = "inventory_file.csv"
+    display_inventory(inventory)
+    add_to_inventory(inventory, dragon_loot) 
+    export_inventory(filename)
+    import_inventory(filename)
+    print_table("count,desc")
 
-#main
 #definitions
 inventory = {'rope': 1, 
             'torch': 6, 
             'gold coin': 42, 
             'dagger': 1, 
             'arrow': 12}
-dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-filename = "input_file.csv"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
-display_inventory(inventory)
-add_to_inventory(inventory, dragon_loot) 
-import_inventory(dir_path) #call import fun without arg, reach default
-export_inventory(dir_path)
-print_table("count,desc")
+demo()
